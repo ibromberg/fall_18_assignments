40ad7d643c789d38e229078445cd126f235a54a4
Ilana Bromberg <ilana.bromberg@stonybrook.edu>
2018-11-29 09:43:49 -0500

    some bugs still - work in progress - basic format/structure is here

--- advection.py
+++ advection.py
@@ -1,42 +1,56 @@
 """
 Reading Class Assignment
 Advection Code
+
+a = 0 if x < 1/3
+  = 1 if 1/3 <= x < 2/3
+  = 0 if 2/3 <= x
+  
+  u_i^n = u(x_i,t^n)
+  
 """
 import matplotlib.pyplot as plt
-import numpy as np
 
 # ------- variables ----------
-C = 0.8 # constant
+C = 0.8 # CFL number
 u = 1.0 # advection speed
-nc = 100 # number of cells
+nc = 100 # number of zones
+xmax = 1.0
+xmin = 0.0
+# loop variables
+i = 0
+t = 0
+
+#x = np.linspace(-1,1,num=nc) # array of x values - includes ghost cell
 x = []
-y = []
+f_th = [] # tophat
+f_new = []
 
-# ------------- make the 1d grid -------------------
+dx = (xmax-xmin)/nc # grid interval
+dt = dx*C / u # time interval
+tmax = (xmax-xmin)/u # maximum time
+max_step = tmax/dt # total number of time steps
+x_3 = (xmax-xmin) / 3.0 # split grid into thirds for top hat
 
-def grid(nc):
-    num_cells = nc
-    x = np.linspace(0.0,1.0,num=num_cells)
-    np.append(x,x[0])
-    return x
-
-# ---------- make the tophat ----------
+# make the grid
+for i in range (0,nc):
+    x.append( xmin + float(i)*dx )
     
-def tophat(x,y,nc):    
-    i = 0    
-    num_cells = nc
-    for i in range(0,num_cells):
-        if x[i]>0.3 and x[i]<0.7:
-            y.append(1.0)
-        else:
-            y.append(0.0)
-                
-    return (y)
+# create top hat by splitting grid
+for i in range (0,nc):
+    f_th.append(0.0)
+    if(x[i]>x_3 and x[i]<2*x_3):
+        f_th.append(1.0)
+f_th.append(f_th[0]) # add ghost cell
 
-# ------------- plotting ---------------
-
-x = grid(nc)
-y = tophat(x,y,nc)
-
-plt.plot(x,y)
-plt.title("Tophat")
+# go through time
+for t in range(0,int(max_step)):
+    # loop through mesh to get soln after one dt time step
+    for i in range(0,nc):
+        f_new.append(f_th[i] - (C*(f_th[i] - f_th[i-1]))) # upwind
+        # f_new.append( f_th[i] - (0.5*C*(f_th[i+1] - f_th[i-1])) ) # FTCS
+    f_new.append(f_new[0])
+    
+# plot time
+    
+plt.plot(x,f_th)
